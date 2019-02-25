3ad5406ed7d1a49981888e5c39135272669e8e98
Khushal Agarwal <39884168+khushal87@users.noreply.github.com>
2019-02-25 19:11:07 +0530

    Update play.js

--- nodejs/play.js
+++ nodejs/play.js
@@ -1,10 +1,16 @@
-//core js syntax
-var name="khushal agarwal";
-var age=18;
-var hashobbies=true;
+let name="khushal agarwal"; //usage of let keyword 
+const age=18; //usage of const keyword
+var hashobbies=true; //usage of var keyword 
+name="Khushal Agarwal"; //trying to see if name changes after using let keyword 
 console.log(name);
-function summarizeuser(username,userage,userhashobbies)
+function summarizeuser(username,userage,userhashobbies) //a simple JS function implementation
 {
-    return ('UserName is-' + username+'Userage is-'+userage+'Userhashobbies-'+userhashobbies);
+    return ('UserName is-' + username+' Userage is- '+userage+' Userhashobbies-'+userhashobbies);
 }
-console.log(summarizeuser(name,age,hashobbies));
+const summerize=(username,userage,userhashobbies) =>{
+    return ('UserName is-' + username+' Userage is- '+userage+' Userhashobbies-'+userhashobbies);  //a arrow function
+};
+const add = (a,b) => (a+b);
+console.log(summarizeuser(name,age,hashobbies));   //a simplified version of arrow function
+console.log(summerize(name,age,hashobbies));
+console.log(add(2,age));
