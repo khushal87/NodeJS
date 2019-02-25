3baa709adca3a7f00b232de1837e7904c9fd9b80
Khushal Agarwal <39884168+khushal87@users.noreply.github.com>
2019-02-25 18:46:59 +0530

    changes

--- nodejs/firstapp.js
+++ nodejs/firstapp.js
@@ -1,2 +1,3 @@
+//a function which allows you to create a new file using js just by importing (fs) in nodejs
 const fs=require('fs');
-fs.writeFileSync('hello.txt','Hey this is khushal agarwal');
+fs.writeFileSync('hello.txt','Hey this is NodeJS world');
