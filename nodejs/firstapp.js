34715a9b0e235cc54e11965e2c2d2b20d0497d5e
Khushal Agarwal <39884168+khushal87@users.noreply.github.com>
2019-02-25 18:45:39 +0530

    Initial commit

---
+++ nodejs/firstapp.js
@@ -0,0 +1,2 @@
+const fs=require('fs');
+fs.writeFileSync('hello.txt','Hey this is khushal agarwal');
---
+++ nodejs/play.js
@@ -0,0 +1,9 @@
+var name="khushal agarwal";
+var age=18;
+var hashobbies=true;
+console.log(name);
+function summarizeuser(username,userage,userhashobbies)
+{
+    return ('UserName is-' + username+'Userage is-'+userage+'Userhashobbies-'+userhashobbies);
+}
+console.log(summarizeuser(name,age,hashobbies));
