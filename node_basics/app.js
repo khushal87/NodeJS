a4e43d1364f2193964861706bef3be8de6f7a57a
Khushal Agarwal <39884168+khushal87@users.noreply.github.com>
2019-02-27 09:18:25 +0530

    server added

---
+++ node_basics/app.js
@@ -0,0 +1,20 @@
+//we do it by first importing the module
+//then to create the screver we use the method createSever.
+//it generally takes two arguments one is the request listener with request and response and the
+const http = require('http'); 
+// function reqlisten(req,res)
+// {                                                //a way to define a request listener
+
+// }
+
+const server=http.createServer(function(req,res)
+{
+    console.log(req.url,req.method,req.headers);                                                //using standard functions
+});
+
+server.listen(3000);
+
+// http.createServer((req,res) =>
+// {
+//     console.log(req);                                creating the srever request using aarrow functions using arrow function 
+// });
