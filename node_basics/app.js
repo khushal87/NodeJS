77774f1cec18b1e7f2d77959bc6fce7d5461e229
Khushal Agarwal <39884168+khushal87@users.noreply.github.com>
2019-02-28 17:45:11 +0530

    Update app.js

--- node_basics/app.js
+++ node_basics/app.js
@@ -1,43 +1,6 @@
-//we do it by first importing the module
-//then to create the server we use the method createSever.
-//it generally takes two arguments one is the request listener with request and response and the
+
 const http = require('http'); 
-const fs= reuqire('fs');
-// function reqlisten(req,res)
-// {                                                //a way to define a request listener
-
-// }
-
-const server=http.createServer(function(req,res)
-{
-    const url=req.url;
-    if(url==='/')                                                          //if '/' this code is executed or else the code following the curly braces is been processed 
-    {
-    res.setHeader('Content-Type','text/html');                             //used to set header(sort of a metadata)
-    res.write("<html>");
-    res.write("<head><title>Enter Message</title></head>");                //html code
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-    res.write("</html>") ;  
-    return res.end();                                                      //we have done it because without it the lines after the if statement is executed and which will create a error.
-    }
-    if(url==='/message'&&method==='POST')
-    {
-        fs.writeFileSync('message.txt','Dummy');
-        res.statusCode=302;
-        res.setHeader('Location','/');                                          //getting the user input in a different file of .txt extension
-        return res.end();
-    }
-    res.setHeader('Content-Type','text/html');   //used to set header(sort of a metadata)
-    res.write("<html>");
-    res.write("<head><title>My first page</title></head>");                //html code
-    res.write("<body><h1>Hello from my node js server.</h1></body>");
-    res.write("</html>") ;  
-    res.end();                                                             //using standard functions
-});
-
+const routes=require('./routes');
+const server=http.createServer(routes);
+    
 server.listen(3000);
-
-// http.createServer((req,res) =>
-// {
-//     console.log(req);                                                   // creating the srever request using aarrow functions using arrow function 
-// });
