ab6e043d8c2e9d829c029bd71444d0a3ed542f77
Khushal Agarwal <39884168+khushal87@users.noreply.github.com>
2019-03-01 19:22:26 +0530

    Update app.js

--- node_basics/app.js
+++ node_basics/app.js
@@ -1,6 +1,9 @@
 
 const http = require('http'); 
-const routes=require('./routes');
-const server=http.createServer(routes);
-    
+//const routes=require('./routes');                         used before exxpressJS
+//const server=http.createServer(routes);
+const express=require('express');           //to import express js
+
+const app=express();                    //execute express as a function
+
 server.listen(3000);
