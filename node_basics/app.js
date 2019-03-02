81045b8177791917c1e0f0f23cd801a65e844bc3
Khushal Agarwal <39884168+khushal87@users.noreply.github.com>
2019-03-02 18:32:45 +0530

    added request

--- node_basics/app.js
+++ node_basics/app.js
@@ -1,9 +1,17 @@
 
 const http = require('http'); 
-//const routes=require('./routes');                         used before exxpressJS
-//const server=http.createServer(routes);
 const express=require('express');           //to import express js
 
 const app=express();                    //execute express as a function
 
+const server=http.createServer(app);        //pass app here
+app.use((req,res,next )=> {
+    console.log("In the middleware!");
+    next();                     //Allows the request to continue to the next middleware.
+});
+app.use((req,res,next )=> {
+    console.log("In another middleware!");              //we see that the next app.use is not executed as we need a next() method to allow the request to travel on to the next middleware.
+});
 server.listen(3000);
+
+ 
