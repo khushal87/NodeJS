b3f6d80005cf90c6052a718b1eb16e025f40537a
Khushal Agarwal <39884168+khushal87@users.noreply.github.com>
2019-03-03 20:32:41 +0530

    Update app.js

--- node_basics/app.js
+++ node_basics/app.js
@@ -1,6 +1,7 @@
 //const http = require('http');                                       //after importing expressjs we can remove our http also as we dont need it                                       
 const express=require('express');                                    //to import express js
 const bodyParser=require('body-parser');
+const path=require('path');
 const adminRoutes=require('./routes/admin');
 const shopRoutes=require('./routes/shop');
 const app=express();                                                    //execute express as a function
@@ -10,11 +11,15 @@ const app=express();                                                    //execut
 // });
 
 app.use(bodyParser.urlencoded({extended:false}));                      //used to parse the body.It(urlencoded()) can basically parse body,form-data etc but not files.
+app.use(express.static(path.join(__dirname,'public')));             //serving file statically   
 app.use(adminRoutes);                                                  //order matters
 app.use(shopRoutes);
 // const server=http.createServer(app);                                 //pass app here
 // server.listen(3000);
-
+app.use('/',(req,res,next) =>
+{
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));                 //if the page accessed is not found we return the error with error code 404 which is commen for page not found 
+});
 app.listen(3000);
 
  
