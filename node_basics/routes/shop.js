0a0679ac09838b531eea617eebce99145369a98d
Khushal Agarwal <39884168+khushal87@users.noreply.github.com>
2019-03-03 13:34:41 +0530

    Update shop.js

--- node_basics/routes/shop.js
+++ node_basics/routes/shop.js
@@ -1,7 +1,12 @@
+const path=require('path');    
 const express=require('express');                                    //to import express js
 const router=express.Router();
-router.use('/',(req,res,next )=> {
+// router.use('/',(req,res,next )=> {
+//     // console.log("In another middleware!");                           //we see that the next app.use is not executed as we need a next() method to allow the request to travel on to the next middleware.
+//      res.send('<h1>Hello from express!</h1>');                          //res.send() a utility which allows us to send/respond  any data
+//  });
+router.get('/',(req,res,next )=> {
     // console.log("In another middleware!");                           //we see that the next app.use is not executed as we need a next() method to allow the request to travel on to the next middleware.
-     res.send('<h1>Hello from express!</h1>');                          //res.send() a utility which allows us to send/respond  any data
- });
+     res.sendFile(path.join(__dirname,'../','views','shop.html'));                          //res.send() a utility which allows us to send/respond  any data
+ });    //here we send html file using sendFile utility.
  module.exports=router;
