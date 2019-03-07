9f5881f823e462e45c14877f7381dd10fc09bbc3
Khushal Agarwal <39884168+khushal87@users.noreply.github.com>
2019-03-07 18:02:27 +0530

    Update app.js

--- node_basics/app.js
+++ node_basics/app.js
@@ -1,5 +1,5 @@
 //const http = require('http');                                       //after importing expressjs we can remove our http also as we dont need it                                       
-const express=require('express');                                    //to import express js
+const express=require('express');                                     //to import express js
 const bodyParser=require('body-parser');
 const path=require('path');
 const adminRoutes=require('./routes/admin');
@@ -11,10 +11,10 @@ const app=express();                                                    //execut
 // });
 
 app.use(bodyParser.urlencoded({extended:false}));                      //used to parse the body.It(urlencoded()) can basically parse body,form-data etc but not files.
-app.use(express.static(path.join(__dirname,'public')));             //serving file statically   
+app.use(express.static(path.join(__dirname,'public')));                //serving file statically   
 app.use(adminRoutes);                                                  //order matters
 app.use(shopRoutes);
-// const server=http.createServer(app);                                 //pass app here
+// const server=http.createServer(app);                                //pass app here
 // server.listen(3000);
 app.use('/',(req,res,next) =>
 {
