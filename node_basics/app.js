9d8066494b18a6fee3a3d3027776486fb8f4f267
Khushal Agarwal <39884168+khushal87@users.noreply.github.com>
2019-03-10 00:35:33 +0530

    Update app.js

--- node_basics/app.js
+++ node_basics/app.js
@@ -5,6 +5,9 @@ const path=require('path');
 const adminData=require('./routes/admin');
 const shopRoutes=require('./routes/shop');
 const app=express();                                                  //execute express as a function
+
+app.set('view engine' , 'pug');                                         //allows us to set any value globally on our express application.Basically allows us to tell express hey for any dynamic templates we're trying to render please use this engine(here pug).
+app.set('views','views');                             //allows us to tell express where to find these dynamic views mentioned above. 2nd parameter is the file name
 // app.use((req,res,next )=> {
 //     console.log("In the middleware!");
 //     next();                                                        //Allows the request to continue to the next middleware in line.
@@ -18,6 +21,7 @@ app.use(shopRoutes);
 // server.listen(3000);
 app.use('/',(req,res,next) =>
 {
-    res.status(404).sendFile(path.join(__dirname,'views','404.html'));                 //if the page accessed is not found we return the error with error code 404 which is commen for page not found 
+    // res.status(404).sendFile(path.join(__dirname,'views','404.html'));                 //if the page accessed is not found we return the error with error code 404 which is commen for page not found 
+    res.status(404).render('404', {pathTitle:'Page not found'});
 });
 app.listen(3000);
