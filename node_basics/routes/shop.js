97dbd4ff05c3ea361a5d7911367ea54050e3f2bc
Khushal Agarwal <39884168+khushal87@users.noreply.github.com>
2019-03-10 00:29:01 +0530

    Update shop.js

--- node_basics/routes/shop.js
+++ node_basics/routes/shop.js
@@ -7,10 +7,9 @@ const router=express.Router();
 
 
 
-router.get('/',(req,res,next )=> {
-    // console.log("In another middleware!");                           //we see that the next app.use is not executed as we need a next() method to allow the request to travel on to the next middleware.
-    console.log('shop.js',adminData.products);
-     res.sendFile(path.join(rootDir,'views','shop.html'));                          //res.send() a utility which allows us to send/respond  any data
- });                                                            //path join detects the operating system and then manually provid ethe correct path
+router.get('/',(req,res,next )  => {
+    const products = adminData.products;
+    res.render('shop',{prods : products,docTitle : 'Shop' , path:'/',pathTitle:'Shop'});                                //res.render is used to render the templating file it will have first argument as the file name and the other arguments are the arguments of the feature which we give to render dynamic template
+    });                                                            //path join detects the operating system and then manually provid ethe correct path
 
  module.exports=router;                                                    //to export router  
