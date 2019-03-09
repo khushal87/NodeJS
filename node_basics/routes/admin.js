8bd32e384ae8524d838c8e56f4839d3cf5894dd5
Khushal Agarwal <39884168+khushal87@users.noreply.github.com>
2019-03-10 00:27:36 +0530

    Update admin.js

--- node_basics/routes/admin.js
+++ node_basics/routes/admin.js
@@ -2,6 +2,7 @@ const express=require('express');                                    //to import
 const path=require('path');
 const router=express.Router();                                        //this is like a mini express app tied to other express app or pluggable to the other express app.
 const rootDir=require('../util/path.js');
+
 // router.get('/add-product',(req,res,next )=> {           
 //     res.send('<form action="/product" method="POST" ><input type="text" name="title"><button type="submit">Add Product</form>');               //remember middleware execution in express goes from top to bottom so this route is defined first or else '/' would mislead us to wrong page. 
 // });
@@ -11,9 +12,10 @@ const products=[];
 // /admin/add-product => GET
 router.get('/add-product',(req,res,next )=> {
     // console.log("In another middleware!");                           //we see that the next app.use is not executed as we need a next() method to allow the request to travel on to the next middleware.
-     res.sendFile(path.join(rootDir,'views','add-product.html'));                          //res.send() a utility which allows us to send/respond  any data
+     //res.sendFile(path.join(rootDir,'views','add-product.html'));                          //res.send() a utility which allows us to send/respond  any data
+     res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product',pathTitle:'Add Product'})      //2nd key value pair (path) is for defining the path for which the following code will be rendered
  }); 
- 
+
 // /admin/add-product => POST
 router.post('/add-product',(req,res,next)=> {                                  //there are two methods get and post.Here we are getting a post data or working with post request thus we need app.post
     products.push({ title: req.body.title });
