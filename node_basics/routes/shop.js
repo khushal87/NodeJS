870a31a4fe99943ca5308a923595b05ee3e24949
Khushal Agarwal <39884168+khushal87@users.noreply.github.com>
2019-03-09 19:23:47 +0530

    Update shop.js

--- node_basics/routes/shop.js
+++ node_basics/routes/shop.js
@@ -1,12 +1,16 @@
-const path=require('path');    
+const path=require('path');                                            //we need this as the directory name doesnt work in form of slash so we use it.
 const express=require('express');                                    //to import express js
+const rootDir=require('../util/path.js')
+const adminData=require('./admin');
+
 const router=express.Router();
-// router.use('/',(req,res,next )=> {
-//     // console.log("In another middleware!");                           //we see that the next app.use is not executed as we need a next() method to allow the request to travel on to the next middleware.
-//      res.send('<h1>Hello from express!</h1>');                          //res.send() a utility which allows us to send/respond  any data
-//  });
+
+
+
 router.get('/',(req,res,next )=> {
     // console.log("In another middleware!");                           //we see that the next app.use is not executed as we need a next() method to allow the request to travel on to the next middleware.
-     res.sendFile(path.join(__dirname,'../','views','shop.html'));                          //res.send() a utility which allows us to send/respond  any data
- });    //here we send html file using sendFile utility.
- module.exports=router;
+    console.log('shop.js',adminData.products);
+     res.sendFile(path.join(rootDir,'views','shop.html'));                          //res.send() a utility which allows us to send/respond  any data
+ });                                                            //path join detects the operating system and then manually provid ethe correct path
+
+ module.exports=router;                                                    //to export router  
